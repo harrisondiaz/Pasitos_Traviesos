ca05828167e25d5bdae4c74f37d142bdf9188c60
Harrison Diaz <harryjhoi01@gmail.com>
2024-03-23 19:11:45 -0500

    Update font styles and dependencies

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/flowbite/**/*.js'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+				
+			},
+		},
 	},
 	plugins: [require('flowbite/plugin')],
 }
